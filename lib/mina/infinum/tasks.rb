ce85b57ef99ae99b1bf6f8456694516737d55dfa
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2020-06-02 15:04:23 +0200

    Add task for creating Sidekiq assets symlink

--- lib/mina/infinum/tasks.rb
+++ lib/mina/infinum/tasks.rb
@@ -1,4 +1,5 @@
 set :service_manager, :upstart
+set :sidekiq_web_namespace, nil
 
 desc 'Restart application'
 task :restart_application do
@@ -13,4 +14,12 @@ namespace :background_workers do
       background_worker(state)
     end
   end
+end
+
+task :link_sidekiq_assets do
+  custom_assets_path = File.join('./', 'public', fetch(:sidekiq_web_namespace).to_s)
+
+  command "bundle_path=\"$(RAILS_ENV=#{fetch(:rails_env)} #{fetch(:bundle_bin)} show sidekiq)\""
+  command "mkdir -p #{custom_assets_path}"
+  command "ln -nfs $bundle_path/web/assets #{custom_assets_path}/sidekiq"
 end
