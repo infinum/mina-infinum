2dfcfc66a3b629dd191daf4eb88f1a31273c639c
Lovro Bikic <lovro.bikic@infinum.hr>
2025-05-26 17:58:38 +0200

    Remove db:proxy args

--- lib/mina/infinum/ecs/db.rb
+++ lib/mina/infinum/ecs/db.rb
@@ -2,7 +2,7 @@ require 'mina/infinum/ecs/aws'
 
 namespace :db do
   desc 'Forward remote DB port to local port through an SSM session'
-  task proxy: ['aws:profile:check'] do |_, args|
+  task proxy: ['aws:profile:check'] do
     ensure!(:aws_bastion_id)
     ensure!(:db_host)
 
@@ -10,7 +10,7 @@ namespace :db do
     bastion_id = fetch(:aws_bastion_id)
     host = fetch(:db_host)
     remote_port = fetch(:db_port, 5432)
-    local_port = args.to_a.fetch(0) { fetch(:db_tunnel_port, 9999) }
+    local_port = fetch(:db_tunnel_port, 9999)
 
     Kernel.exec squish(<<~CMD)
       aws ssm start-session
