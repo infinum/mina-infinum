6996ffede2a976fbd70e3593e02333a1ea5b266b
Lovro Bikic <lovro.bikic@infinum.hr>
2025-10-01 10:26:14 +0200

    start -> since, end -> until

--- lib/mina/infinum/ecs/logs.rb
+++ lib/mina/infinum/ecs/logs.rb
@@ -7,33 +7,37 @@ desc <<~TXT
 
   Logs are printed from :log_group, which is a CloudWatch log group name.
 
-  Logs are fetched in range :start to :end (both inclusive). Both values must be
-  parseable by `Time.parse` (e.g. ISO8601 timestamps). :start is required,
-  :end is optional (if omitted, value will be current time).
+  Logs are fetched in range :since to :until (both inclusive). Both values must be
+  parseable by `Time.parse` (e.g. ISO8601 timestamps). :since is required,
+  :until is optional (if omitted, value will be current time).
 
   Some examples:
-  # all logs from 26.9.2025. 14:00 (local time zone) until now
-  $ mina logs start="2025-09-26 14:00"
+  # all logs since 26.9.2025. 14:00 (local time zone) until now
+  $ mina logs since="2025-09-26 14:00"
 
-  $ mina logs start="2025-09-26 14:00" end="2025-09-26 15:00"
+  # all logs on 26.9.2025. between 14:00 and 15:00
+  $ mina logs since="2025-09-26 14:00" until="2025-09-26 15:00"
+
+  # all logs between 14:00 and 15:00 today
+  $ mina logs since="14:00" until="15:00"
 
   # UTC time zone
-  $ mina logs start="2025-09-26T14:00Z"
+  $ mina logs since="2025-09-26T14:00Z"
 TXT
 task logs: ['aws:profile:check'] do
   ensure!(:log_group)
-  ensure!(:start)
+  ensure!(:since)
 
-  start_time = Time.parse(fetch(:start))
-  end_time = fetch(:end) ? Time.parse(fetch(:end)) : Time.now
+  since_time = Time.parse(fetch(:since))
+  until_time = fetch(:until) ? Time.parse(fetch(:until)) : Time.now
 
-  puts "Printing logs from #{fetch(:log_group)} in range [#{start_time.iso8601},#{end_time.iso8601}]"
+  puts "Printing logs from #{fetch(:log_group)} in range [#{since_time.iso8601},#{until_time.iso8601}]"
   raw_logs = run_cmd squish(<<~CMD)
     aws logs filter-log-events
       --log-group-name #{fetch(:log_group)}
       --profile #{fetch(:aws_profile)}
-      --start-time #{start_time.strftime('%s%L')}
-      --end-time #{end_time.strftime('%s%L')}
+      --start-time #{since_time.strftime('%s%L')}
+      --end-time #{until_time.strftime('%s%L')}
       --output json
       --query "events[].{timestamp: timestamp, message: message}"
   CMD
@@ -58,13 +62,13 @@ namespace :logs do
     Logs are tailed from :log_group, which is a CloudWatch log group name.
 
     Before new logs are tailed, recent logs are first printed. You can control
-    from what time recent logs are printed with :start. The value can be an
+    from what time recent logs are printed with :since. The value can be an
     ISO 8601 timestamp or a relative time. For example:
-    $ mina logs:tail start="2025-09-26T12:00:00"
+    $ mina logs:tail since="2025-09-26T12:00:00"
     or
-    $ mina logs:tail start="5m"
+    $ mina logs:tail since="5m"
 
-    For more info on accepted :start values, see --since options
+    For more info on accepted :since values, see --since options
     https://docs.aws.amazon.com/cli/latest/reference/logs/tail.html#options
   TXT
   task tail: ['aws:profile:check'] do
@@ -76,7 +80,7 @@ namespace :logs do
         --profile #{fetch(:aws_profile)}
         --follow
         --format short
-        #{"--since #{fetch(:start)}" if fetch(:start)}
+        #{"--since #{fetch(:since)}" if fetch(:since)}
     CMD
   end
 end
